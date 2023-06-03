a1cd306d8a044d7f0c434c1c3f78d6dfcd07a42a
ErickChAg <66395496+ErickChAg@users.noreply.github.com>
2023-06-02 20:58:45 -0600

    initial project

---
+++ OPTProject/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpComponent,
+    SignInComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
