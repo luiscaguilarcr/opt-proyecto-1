9c66f2b99f540a28f49230661e24b3302d2d6ffd
David Robles Pérez <jdrobles0110@gmail.com>
2023-06-08 12:23:57 -0600

    Nuevos Avances
    Conectar el email del usuario recibido en el sign in al form component de las preguntas (En progreso). A razón de lo anterior, estoy corregiendo el userAnswers porque solo me está guardando la última pregunta y es posible que no se esté almacenando en el arreglo por el user:_id (email). (En progreso)

--- OPTProject/src/app/form/form.component.ts
+++ OPTProject/src/app/form/form.component.ts
@@ -4,6 +4,7 @@ import { question } from '../models/question';
 import { Answer } from '../models/answer';
 import { UserAnswer } from '../models/useranswer';
 import { AnswersService } from '../services/answers/answers.service';
+import { user } from '../models/user'; // Importa el modelo 'user'
 
 @Component({
   selector: 'app-formulario',
@@ -15,6 +16,7 @@ export class FormComponent implements OnInit {
   currentQuestionIndex: number = 0;
   selectedAnswer: number = 1; // Valor predeterminado para el primer radio button
   answers: Answer[] = [];
+  user: user = new user(); // Declara e inicializa la propiedad 'user' con un nuevo objeto 'user'
 
   constructor(
     private questionsService: QuestionsService,
@@ -40,35 +42,40 @@ export class FormComponent implements OnInit {
 
   finish() {
     console.log('¡Test terminado!');
-    const selectedQuestion = this.questions[this.currentQuestionIndex];
-    const selectedOption = this.selectedAnswer !== undefined ? this.selectedAnswer : null;
-  
-    let weight: number = selectedOption || 0; // Asignar el valor de selectedOption a weight, o 0 si es null o undefined
-  
-    const answer: Answer = {
-      question_id: selectedQuestion._id,
-      answer: selectedOption,
-      weight: weight
-    };
-    this.answers.push({ ...answer });
-  
-    // Guardar la respuesta de usuario en la API
-    const userAnswer: UserAnswer = {
-      user_id: 'string', // Reemplazar con el ID del usuario
-      ...answer
-    };
-  
-    this.answersService.putAnswers([userAnswer]).subscribe(
+    let userAnswers: UserAnswer[];
+
+    // Obtener el ID del usuario desde la propiedad 'user' en tu componente
+    const userId = this.user.email; // Suponiendo que el email del usuario sea el ID
+
+    userAnswers = this.questions.map((question) => {
+      const selectedOption = this.selectedAnswer !== undefined ? this.selectedAnswer : null;
+      const weight: number = selectedOption || 0;
+
+      const answer: Answer = {
+        question_id: question._id,
+        answer: selectedOption,
+        weight: weight
+      };
+
+      return {
+        user_id: userId,
+        ...answer
+      };
+    });
+
+    console.log('Respuestas seleccionadas:', userAnswers);
+
+    this.answersService.putAnswers(userAnswers).subscribe(
       (response) => {
-        console.log('Respuesta de usuario guardada:', response);
+        console.log('Respuestas de usuario guardadas:', response);
         // Realizar acciones finales o redireccionar a otra página
       },
       (error) => {
-        console.log('Error al guardar respuesta de usuario:', error);
+        console.log('Error al guardar respuestas de usuario:', error);
       }
     );
   }
-  
+
   getOptions(): number[] {
     return Array.from({ length: 10 }, (_, i) => i + 1);
   }
