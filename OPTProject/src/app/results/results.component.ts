b67a7c04e7cd7721b51aa7b6f05c6616964ac31f
Luis Aguilar <laguilar@pernix-solutions.com>
2023-06-12 02:04:10 -0600

    Adding updates for results and home component

--- OPTProject/src/app/results/results.component.ts
+++ OPTProject/src/app/results/results.component.ts
@@ -10,9 +10,10 @@ import {IntelligenceService} from "../services/intelligences/intelligence.servic
 })
 export class ResultsComponent {
   constructor(private resultsService: ResultsService, private router: Router, private intelligencesService: IntelligenceService) {}
-
+  imageUrl: string = "https://www.sca.org/wp-content/uploads/2019/12/clash_lg-1024x682-2.jpg";
   data: any;
-  predominant: any = [];
+  predominants: any = [];
+  users: any = [{"name": "Luis Aguilar 1", "email": "lag1@em.com"}, {"name": "Luis Aguilar 2", "email": "lag2@em.com"}, {"name": "Luis Aguilar 3", "email": "lag3@em.com"}, {"name": "Luis Aguilar 4", "email": "lag4@em.com"}, {"name": "Luis Aguilar 5", "email": "lag5@em.com"}];
 
   ngOnInit(): void {
     this.data = this.resultsService.results
@@ -25,7 +26,7 @@ export class ResultsComponent {
           this.data.items.forEach((item: any, index: number) => {
             if(index <= 2) {
               response.forEach((intelligence:any) => {
-                intelligence.name === item.intelligence_name ? this.predominant.push({"name": item.intelligence_name, "description": intelligence.description}) : null;
+                intelligence.name === item.intelligence_name ? this.predominants.push({"name": item.intelligence_name, "description": intelligence.description}) : null;
               })
             }
           })
@@ -36,7 +37,10 @@ export class ResultsComponent {
       }
       );
     }
+  }
 
+  goHome() {
+    this.router.navigate(["/home"]);
   }
 
 }
